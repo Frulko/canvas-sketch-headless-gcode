0783563e3dd673c9e00cd93e7bc006d56e40abfa
Guillaume <guillaume.dumoulin@hotmail.fr>
2020-09-12 20:20:04 +0200

    first commit

---
+++ GCodeFile.js
@@ -0,0 +1,172 @@
+const { mapRange } = require('canvas-sketch-util/math')
+const { clipPolylinesToBox } = require('canvas-sketch-util/geometry')
+const { contain } = require('./intrinsic-scale')
+
+const defaultConfig = {
+  feedRate: 8000, // G1 movement (drawing speed)
+  seekRate: 8000, // G0 movement (no drawing speed)
+  onCommand: 'M03S20',
+  offCommand: 'M03S0',
+  powerDelay: 0.2,
+  fileName: 'sketch',
+  paperSize: [210, 297],
+  margin: 10,
+  flipX: false,
+  flipY: false
+}
+
+class GCodeFile {
+  constructor(config) {
+    this.config = {
+      ...defaultConfig,
+      ...config
+    }
+    this.normalizeMargin()
+    this.updateDrawArea()
+    this.clear()
+  }
+
+  clear(){
+    this.layers = []
+    this.addLayer()
+  }
+
+  addLayer(name = ''){
+    this.layers.push({
+      name,
+      gcode: ''
+    })
+    this.currentLayer = this.layers.length - 1
+  }
+
+  normalizeMargin() {
+    if (typeof this.config.margin === 'number') {
+      this.config.margin = [this.config.margin, this.config.margin, this.config.margin, this.config.margin]
+    } else if (Array.isArray(this.config.margin)) {
+      if (this.config.margin.length === 2) {
+        this.config.margin[2] = this.config.margin[0]
+        this.config.margin[3] = this.config.margin[1]
+      }
+    } else {
+      throw new Error('Margin option can be a number or an array.')
+    }
+  }
+
+  updateConfig(config = {}) {
+    this.config = {
+      ...this.config,
+      ...config
+    }
+
+    this.normalizeMargin()
+    this.updateDrawArea()
+  }
+
+  updateDrawArea() {
+    this.drawArea = [
+      this.config.paperSize[0] - this.config.margin[0] - this.config.margin[2],
+      this.config.paperSize[1] - this.config.margin[1] - this.config.margin[3]
+    ]
+  }
+
+  updateCoordsArea(width, height) {
+    this.coordsWidth = width
+    this.coordsHeight = height
+    this.updateDrawCoords()
+  }
+
+  updateDrawCoords() {
+    const {
+      offsetX,
+      offsetY,
+      width,
+      height
+    } = contain(this.drawArea[0], this.drawArea[1], this.coordsWidth, this.coordsHeight)
+    this.offsetX = offsetX
+    this.offsetY = offsetY
+    this.drawWidth = width
+    this.drawHeight = height
+  }
+
+  mapCoordsToDrawArea(x, y) {
+    if (!this.coordsWidth) {
+      throw new Error('Must call "updateCoordsArea" passing width and height of your coordinate system!')
+    }
+    if (!this.coordsHeight) {
+      throw new Error('Must call "updateCoordsArea" passing width and height of your coordinate system!')
+    }
+
+    const coords = {
+      x: this.config.margin[0] + this.offsetX + mapRange(x, 0, this.coordsWidth, 0, this.drawWidth, true),
+      y: this.config.margin[1] + this.offsetY + mapRange(y, 0, this.coordsHeight, 0, this.drawHeight, true)
+    }
+
+    if (this.config.flipX) {
+      coords.x = this.config.paperSize[0] - coords.x
+    }
+
+    if (this.config.flipY) {
+      coords.y = this.config.paperSize[1] - coords.y
+    }
+
+    coords.x = parseFloat(coords.x.toFixed(3))
+    coords.y = parseFloat(coords.y.toFixed(3))
+
+    return coords
+  }
+
+  moveTo(x, y) {
+    const coords = this.mapCoordsToDrawArea(x, y)
+    this.layers[this.currentLayer].gcode += `\n${this.config.offCommand}\nG4 P${this.config.powerDelay}\nG0 X${coords.x} Y${coords.y}\n${this.config.onCommand}\nG4 P${this.config.powerDelay}`
+  }
+
+  drawLine(x, y) {
+    const coords = this.mapCoordsToDrawArea(x, y)
+    this.layers[this.currentLayer].gcode += `\nG1 X${coords.x} Y${coords.y}`
+  }
+
+  addPolylines(polylines) {
+    const lines = clipPolylinesToBox(
+      polylines,
+      [0, 0, this.coordsWidth, this.coordsHeight]
+    )
+
+    lines.forEach(l => {
+      l.forEach((point, i) => {
+        if (i == 0) {
+          this.moveTo(point[0], point[1])
+        } else {
+          this.drawLine(point[0], point[1])
+        }
+      })
+    })
+  }
+
+  beginFile() {
+    return `G0 F${this.config.seekRate}\nG1 F${this.config.feedRate}\nG90\nG21`
+  }
+
+  closeFile() {
+    return `\n${this.config.offCommand}\nG4 P1\nG0 X0 Y0\nG4 P1`
+  }
+
+  downloadFile() {
+    this.layers.forEach(layer => {
+      // from https://stackoverflow.com/a/38019175
+      // const gcodeBlob = new Blob([this.beginFile()+layer.gcode+this.closeFile()], { type: 'text/plain;charset=utf-8' })
+      // const gcodeUrl = URL.createObjectURL(gcodeBlob)
+      // const downloadLink = document.createElement('a')
+      // downloadLink.href = gcodeUrl
+      // downloadLink.download = this.config.fileName + (layer.name ? `-${layer.name}` : '') + '.gcode'
+      // document.body.appendChild(downloadLink)
+      // downloadLink.click()
+      // document.body.removeChild(downloadLink)
+      // console.log(layer.gcode);
+    })
+
+    const layer = this.layers[0];
+    return this.beginFile()+layer.gcode+this.closeFile()
+  }
+}
+
+module.exports = GCodeFile
---
+++ app_headless.js
@@ -0,0 +1,89 @@
+const canvasSketch = require('canvas-sketch');
+const { pathsToSVG, renderPaths, createPath, pathsToPolylines } = require('canvas-sketch-util/penplot');
+const { clipPolylinesToBox } = require('canvas-sketch-util/geometry');
+const Random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
+const {vec2} = require('gl-matrix')
+const fs = require('fs');
+
+
+const GCodeFile = require('./GCodeFile');
+
+
+
+let circlePos = [];
+
+// You can force a specific seed by replacing this with a string value
+const defaultSeed = '';
+
+// Set a random seed so we can reproduce this print later
+Random.setSeed(Date.now());
+
+
+const gCode = new GCodeFile({
+	feedRate: 8000, // G1 movement (drawing speed)
+  seekRate: 8000, // G0 movement (no drawing speed)
+	onCommand: 'M03S90',
+	offCommand: 'M03S20',
+	powerDelay: 0.2,
+	fileName: 'sketch',
+	paperSize: [230, 340], // A4 size in mm
+	margin: 0, // or [10, 10] (top/bottom and left/right margin) or [10,10,10,10] (top/left/bottom/right)
+	flipX: false,
+	flipY: false
+})
+
+const settings = {
+  // canvas,
+  suffix: Random.getSeed(),
+  dimensions: [25,32],
+  orientation: 'portrait',
+  pixelsPerInch: 300,
+  scaleToView: true,
+  units: 'cm'
+};
+
+const sketch = (props) => {
+
+  const { width, height, units } = props;
+  gCode.updateCoordsArea(width, height);
+  // Holds all our 'path' objects
+  // which could be from createPath, or SVGPath string, or polylines
+  const paths = [];
+  let points = [];
+
+  for (let i = 0; i <= 100; i++) {
+    const angle = math.mapRange(i, 0, 100, 0, 2 * Math.PI)
+    const x = width/2 + Math.cos(angle) * 5;
+    const y = height/2 + Math.sin(angle) * 5;
+    points.push([x,y])
+  }
+
+  // [[0,0],[25,0],[25,32],[0,32],[0,0]].forEach((point) => {
+  //   points.push(point);
+  // });
+  const rect = [[0,0],[23,0],[23,34],[0,34],[0,0]];
+  paths.push(rect);
+
+  console.log('path:', paths);
+  // Convert the paths into polylines so we can apply line-clipping
+  // When converting, pass the 'units' to get a nice default curve resolution
+  let lines = pathsToPolylines(paths, { units });
+  console.log('lines', lines);
+
+  // Clip to bounds, using a margin in working units
+  const margin = 0; // in working 'units' based on settings
+  const box = [ margin, margin, width - margin, height - margin ];
+  // lines = clipPolylinesToBox(lines, box);
+
+  gCode.addPolylines(lines)
+
+
+  return gCode.downloadFile();
+};
+
+const svg = sketch({width: 23, height:34,  units: 'cm', optimize: false});
+fs.writeFile('./output.gcode', svg, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+});
